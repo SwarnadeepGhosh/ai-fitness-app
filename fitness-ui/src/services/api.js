88558705bb68077a67b55936d0dc4b86b9d338e9
Swarnadeep Ghosh <swarna.lyf@gmail.com>
2026-07-22 16:04:52 +0530

    enabled externalize configuration in UI

--- fitness-ui/src/services/api.js
+++ fitness-ui/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const API_URL = 'http://localhost:8080/api';
+// const API_URL = 'http://localhost:8080/api';
 
 const api = axios.create({
-    baseURL:API_URL
+    baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
